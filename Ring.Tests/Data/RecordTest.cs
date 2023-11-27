23f7b54d09e1e755ae78d76b3925bb3b97ca84b0
AC\gnhstp <gnhstp@forem.be>
2023-11-27 13:27:49 +0100

    Added Record structure

---
+++ Ring.Tests/Data/RecordTest.cs
@@ -0,0 +1,67 @@
+﻿using Ring.Tests.Schema.Extensions;
+using Record = Ring.Data.Record;
+using DbSchema = Ring.Schema.Models.Schema;
+using Ring.Schema.Enums;
+using Ring.Schema.Extensions;
+using Ring.Schema.Models;
+using System.Runtime.InteropServices;
+
+namespace Ring.Tests.Data;
+
+public sealed class RecordTest : BaseExtensionsTest
+{
+    private readonly DbSchema _schema;
+
+    public RecordTest()
+    {
+        var metaList = GetSchema1();
+        var meta = new Meta("Test");
+        _schema = MetaExtensions.ToSchema(metaList, DatabaseProvider.PostgreSql) ??
+                    MetaExtensions.GetEmptySchema(meta, DatabaseProvider.PostgreSql);
+    }
+
+    [Fact]
+    internal void Equal_EmptyRecord_True()
+    {
+        // arrange 
+        var rcd = new Record();
+        var OtherRcd = new Record();
+
+        // act 
+        var result = rcd.Equals(OtherRcd);
+
+        // assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    internal void Equal_Null_False()
+    {
+        // arrange 
+        var rcd = new Record();
+
+        // act 
+        var result = rcd.Equals(null);
+
+        // assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    internal void Equal_ClassRecord_True()
+    {
+        // arrange 
+        var classTable = _schema.GetTable("feat");
+        Assert.NotNull(classTable);
+        var rcd1 = new Record(classTable);
+        var rcd2 = new Record(classTable);
+
+        // act 
+        var result = rcd1.Equals(rcd2);
+
+        // assert
+        Assert.True(result);
+    }
+
+}
+
---
+++ Ring/Data/Record.cs
@@ -0,0 +1,47 @@
+﻿using Ring.Schema.Models;
+
+namespace Ring.Data;
+
+public struct Record : IEquatable<Record>
+{
+    private string?[]? _data; // should be instanciate when record type is defined
+    private Table? _type;
+
+    /// <summary>
+    ///     Ctor
+    /// </summary>
+    public Record()
+    {
+        _type = null;
+        _data = null;
+    }
+    internal Record(Table type)
+    {
+        _type = type;
+        _data = null;
+    }
+
+    public static bool operator==(Record left, Record right) => left.Equals(right);
+    public static bool operator!=(Record left, Record right) => !(left==right);
+    public readonly bool Equals(Record other)
+    {
+        if (ReferenceEquals(_type, other._type)) {
+            if (_data==null ^ other._data==null) return false;
+            if (_data!=null && other._data!=null) {
+                var i = 0;
+                while (i<_data.Length)
+                {
+                    if (!string.Equals(_data[i], other._data[i], StringComparison.Ordinal)) return false;
+                    ++i;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+    public override readonly bool Equals(object? obj) => obj != null && obj is Record record && Equals(record);
+    public override int GetHashCode()
+    {
+        throw new NotImplementedException();
+    }
+}
