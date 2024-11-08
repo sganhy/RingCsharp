bad9f00c6879d2cfbb6e9d09ae6da7f0ea0846f6
IRISAD\sganhy <sganhy@hotmail.com>
2024-11-08 13:36:58 +0100

    Reformat sources #21

--- Ring/Util/Enums/ResourceType.cs
+++ Ring/Util/Enums/ResourceType.cs
@@ -10,42 +10,42 @@ internal enum ResourceType : short
 	SQLServerReservedKeyWord = 36,
 	SQLiteReservedKeyWord = 49,
 
-    /// <summary>
-    /// Bulk alter/ retrieve / save ressources
-    /// </summary>
-    BulkAlterInvalidFieldName = 70,
-    BulkAlterInvalidTableName = 71,
+	/// <summary>
+	/// Bulk alter/ retrieve / save ressources
+	/// </summary>
+	BulkAlterInvalidFieldName = 70,
+	BulkAlterInvalidTableName = 71,
 
-    /// <summary>
-    /// Record ressources
-    /// </summary>
-    RecordWrongRelationType = 99,
-    RecordUnkownRelationName = 100,
-    RecordUnkownFieldName = 101,
+	/// <summary>
+	/// Record ressources
+	/// </summary>
+	RecordWrongRelationType = 99,
+	RecordUnkownRelationName = 100,
+	RecordUnkownFieldName = 101,
 	RecordUnkownRecordType = 102,
 	RecordWrongStringFormat = 103,
 	RecordValueTooLarge = 104,
 	RecordWrongBooleanValue = 105,
 	RecordCannotConvert = 106,
 
-    /// <summary>
-    /// Miscellaneous
-    /// </summary>
-    UnRepresentableDateTime = 107,
+	/// <summary>
+	/// Miscellaneous
+	/// </summary>
+	UnRepresentableDateTime = 107,
 	NotSupportedInputDateTime = 108,
 	InvalidBase64String = 109,
 	FieldIsMandatory = 110,
 
-    /// <summary>
-    /// Sql operations description
-    /// </summary>
-    CreateTableNotOk = 151,
-    CreateTableOk = 152,
+	/// <summary>
+	/// Sql operations description
+	/// </summary>
+	CreateTableNotOk = 151,
+	CreateTableOk = 152,
 
-    /// <summary>
-    /// IRingConnection messages 
-    /// </summary>
-    UnsuportedOperation = 200,
-    DdlException = 201,
-    DdlTableCreated = 202
+	/// <summary>
+	/// IRingConnection messages 
+	/// </summary>
+	UnsuportedOperation = 200,
+	DdlException = 201,
+	DdlTableCreated = 202
 }
