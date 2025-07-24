60b20d9e50375e43b81f2181751b8ba9fde11081
c:\coding\Ring <sganhy@hotmail.com>
2025-07-25 00:03:58 +0200

    Fixed InternalsVisibleTo issue 4 SchemaMgr project

--- SchemaMgr/Program.cs
+++ SchemaMgr/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Ring.Schema;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+Meta meta = new Meta();
