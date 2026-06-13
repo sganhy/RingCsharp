c3d7fd28bb9aea2133c6262c3c2401291f56bff4
c:\coding\Ring <sganhy@hotmail.com>
2026-06-13 23:07:39 +0200

    Upgraded nugget Microsoft.NET.Test.Sdk v18.6.0.

--- Ring.Data/Class1.cs
+++ Ring.Data/Class1.cs
@@ -1,7 +1,17 @@
-﻿namespace Ring.Data
-{
-    public class Class1
-    {
+﻿using Ring.Schema;
 
-    }
+
+namespace Ring.Data;
+
+public class Class1
+{
+
+	IConnection connection;
+    Meta _meta;
+	
+	public Class1()
+	{
+		_meta = new Meta();
+	}
+
 }
